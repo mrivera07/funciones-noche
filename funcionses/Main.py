875c2c190ba34a0eabfa34b0748b829d23c98b33
marcela rivera <yinar422@gmail.com>
2024-10-03 18:52:47 -0500

    se crea el proyecto con lel archivo main

---
+++ funcionses/Main.py
@@ -0,0 +1,14 @@
+#ejemlo de una funcion
+def my_function():
+    print("hello from a fuction")
+print("hoy es el quiz")
+
+#invocar la funcion
+my_function()
+
+#Ejemplo funcion con argumentos
+def mostrarNombre (nombre):
+    print("su nombre es :"+ nombre)
+nom =input("ingrese su nombre:")
+#invocar la funcion
+mostrarNombre(nom)
